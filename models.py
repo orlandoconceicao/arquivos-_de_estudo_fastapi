ba5831dc2b53607087828bc39a2ff0da69c82e07
aapolo7 <orlandoconceicao94@gmail.com>
2026-01-27 23:11:06 -0400

    27/06

---
+++ models.py
@@ -0,0 +1,76 @@
+from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float, ForeignKey
+from sqlalchemy.orm import declarative_base 
+from sqlalchemy_utils.types import ChoiceType
+
+db = create_engine("sqlite:///banco.db")
+
+# Conexão do banco
+Base = declarative_base()
+
+# Base do banco de dados
+
+# Usuários
+class Usuario(Base): # O nome da tabela normalmente vai ser o nome da class tudo lower com um s no fin
+    __tablename__ = "usuarios"
+    
+    # Restrições sql
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    nome = Column("nome", String)
+    email = Column("email", String, nullable=False)
+    senha = Column("senha", String)
+    ativo = Column("ativo", Boolean)
+    admin = Column("admin", Boolean, default=False)
+    
+    # Tudo que precisa para criar um user
+    def __init__(self, nome, email, senha, ativo=True, admin=False):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.ativo = ativo
+        self.admin = admin
+    
+# Pedidos
+class Pedido(Base):
+    __tablename__ = "pedidos"
+    
+    # Tupla
+    #STATUS_PEDIDOS = (
+        #(chave, valor),
+    #    ("PENDENTE", "PENDENTE"),
+    #    ("CALNCELADO", "CANCELADO")
+    #    ("FINALIZADO", "FINALIZADO")
+    #)
+    
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    status = Column("status", String) # pendente, cancelado, finalizado
+    usuario = Column("usuario",  ForeignKey("usuarios.id"))
+    preco = Column("preco", Float)
+    #item =
+    
+    def __init__(self, usuario, status="PENDENTE", preco=0): # Quando o banco de dados for criado nao vai ter preço ainda e nem status por issocoloca usuario="pendente e preco+0"
+        self.usuario = usuario
+        self.status = status
+        self.preco = preco
+    
+# Itens de pedido
+class ItemPedido(Base):
+    __tablename__ = "itenspedido"
+    
+    ## talvez crie tupla de sabor ou em outro arq e de tamanhos tambem depois puxa aqui
+    
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    quantidade = Column("quantidade", Integer)
+    sabor = Column("sabor", String)
+    tamanho = Column("tamanho", String)
+    preco_unitario = Column("preco_unitario", Float)
+    pedido = Column("pedido", ForeignKey("pedidos.id"))
+        
+    def __init__(self, quantidade, sabor, tamanho, preco_unitario, pedido):
+        self.quantidade = quantidade
+        self.sabor = sabor
+        self.tamanho = tamanho
+        self.preco_unitario = preco_unitario
+        self.pedido = pedido
+
+
+# Executar e criar os metodos do seu banco
